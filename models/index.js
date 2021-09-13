76c68c1ddd7637199d644b901277ee2676f67f80
Anolla <annehaddad96@yahoo.com>
2021-09-13 14:25:55 +0300

    changes sequelize options

--- models/index.js
+++ models/index.js
@@ -4,18 +4,25 @@ const usersModel = require("../models/usersModel");
 const ticketsModel = require("../models/ticketsModel");
 const { Sequelize, DataTypes } = require("sequelize");
 // const sequelize = new Sequelize(process.env.DATABASE_URL,{});
-let DATABASE_URL =
-  "postgres://uojrxlrtiakavk:bba5fe630a52eef60ea1a6557726df1bab5842369d9f165a91eab3d787970ab1@ec2-54-217-15-9.eu-west-1.compute.amazonaws.com:5432/d151m6s2avllim";
+// let DATABASE_URL =
+//   "postgres://uojrxlrtiakavk:bba5fe630a52eef60ea1a6557726df1bab5842369d9f165a91eab3d787970ab1@ec2-54-217-15-9.eu-west-1.compute.amazonaws.com:5432/d151m6s2avllim";
 //POSTGRES_URI from Config Vars on Heroku
 let sequelizeOptions = {
+  dialect: "postgres",
+  protocol: "postgres",
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    ssl: true,
   },
 };
-let sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+// let sequelizeOptions = {
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// };
+let sequelize = new Sequelize(process.env.DATABASE_URL, sequelizeOptions);
 
 const users = usersModel(sequelize, DataTypes);
 const tickets = ticketsModel(sequelize, DataTypes);
